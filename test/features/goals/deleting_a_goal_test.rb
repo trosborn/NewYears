72eeda067851cef95aac2e67ad7aff1efb85ae77
Alicia French <alienor18@yahoo.com>
2014-03-04 21:02:07 -0800

    Scaffold Goal and fix tests

--- test/features/goals/deleting_a_goal_test.rb
+++ test/features/goals/deleting_a_goal_test.rb
@@ -3,8 +3,7 @@ require "test_helper"
 feature "Deleting a goal" do
   scenario "My goal is bad I want to delete it" do
     visit goals_path
-
-    click_on "Destroy"
-    page.wont_have_content "healthy"
+    click_link( 'Destroy', :match => :first)   
+page.wont_have_content "healthy"
   end
 end
