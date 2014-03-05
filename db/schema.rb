3f470a486092b0be3856b841883c75f9079d02fc
Alicia French <alienor18@yahoo.com>
2014-03-04 21:22:16 -0800

    added missing files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211230655) do
+ActiveRecord::Schema.define(version: 20140303054526) do
+
+  create_table "goals", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "frequency"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
